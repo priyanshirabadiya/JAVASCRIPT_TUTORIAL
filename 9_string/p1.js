44f18279b41caaf157224ede6d3ca4fd370eb077
priyanshirabadiya <139095730+priyanshirabadiya@users.noreply.github.com>
2024-02-22 14:31:12 +0530

    22/2/24

--- 9_string/p1.js
+++ 9_string/p1.js
@@ -24,3 +24,49 @@
 // let txt = 'hello , world';
 // console.log(txt.split("L"));
 // console.log(txt.split(" "));
+
+
+
+// string slice
+// let txt = "12345678910112";
+// let txt2 = "javascript is a language";
+// console.log(txt.slice(2,10));
+// console.log(txt.slice(2,-10));
+// console.log(txt.slice(-1,3)); // this series can't move -10 of 0  and can't go on minus side because JavaScript counts positions from zero.
+// console.log(txt.slice(5));
+// console.log(txt2.slice(-10));
+// console.log(txt2.slice(-10,-5));
+
+
+
+// substring
+// (-)count compulsory from 0
+// let text = "Apple,Banana,Kiwi";
+// let data = text.substring(20, -5); // 20 to 0
+// let data = text.substring(-20, -5); // 20 to 0
+
+// console.log(data);
+
+// substr 
+// let text = "Apple, Banana, Kiwi";
+// let data = text.substr(7);
+// console.log(text.substr(-4));
+
+
+// string replace
+// let text = "Please visit Microsoft! And Microsoft is the best one.";
+// let replaceStr = text.replace("Microsoft", "SkillQode");
+// let replaceStr = text.replaceAll("Microsoft", "SkillQode");
+// console.log(replaceStr);
+
+
+// concat string
+// let txt1 = "hello";
+// let txt2 = "world";
+// console.log(txt1.concat(" ",txt2));
+
+
+// charat
+
+let txt = "hello pc";
+console.log(char);
