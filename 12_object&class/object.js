72912d46063be6363745e646166db3641656e4ed
priyanshirabadiya <139095730+priyanshirabadiya@users.noreply.github.com>
2024-03-01 19:48:42 +0530

    1/3/24

--- 12_object&class/object.js
+++ 12_object&class/object.js
@@ -17,19 +17,22 @@ let person = {
 // console.log(person[a]);
 
 // Add property
-person.gender = "female";
-person.address = {
-    line : "208,varaccha",
+// person.gender = "female";
+// person.address = {
+//     line : "208,varaccha",
     // city = { 
     //     area : "varaccha",
     //     chowk : "yogi chowk"
     // },
-    state : "gujrat"
-}
+//     state : "gujrat"
+// }
 // person.hobby = ['reading','music','dancing','playing'];
 // console.log(person.email,person.address);
 // console.log(person.gender);
 // console.log(person["hobby"]);
+// console.log(person.hobby[2]);
+// console.log(person["hobby"]["2"]);
+
 // console.log(person["address"]);
 // console.log(person["address"]["line"]);
 // console.log(person.address.line);
@@ -37,3 +40,23 @@ person.address = {
 
 // delete person.gender;
 // console.log(person);
+
+
+
+
+
+
+// object create methods
+function hicle(name,maker,engine)
+{
+    this.name = name;
+    this.maker = maker;
+    this.engine = engine;
+}
+let car  = new hicle('GT','BMW','1998cc');
+console.log(car.name);
+console.log(car.maker);
+// Property accessors
+// console.log(car.name);
+// console.log(car.maker);
+// console.log(car['engine']);
