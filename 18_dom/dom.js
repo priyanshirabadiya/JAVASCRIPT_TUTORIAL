9e6918b7791207d1469f5145a98c7ba05b65f190
priyanshirabadiya <139095730+priyanshirabadiya@users.noreply.github.com>
2024-03-08 05:39:45 +0530

    3/8/24

--- 18_dom/dom.js
+++ 18_dom/dom.js
@@ -2,3 +2,62 @@
 // what is dom object model(DOM)
 
 
+// javascript getelementbyID
+
+{
+    // function findElement()
+    {
+        // const element = document.getElementById("id1");
+        // element.innerHTML = "Element found!...";
+        // element.style.color = "red";
+        // document.getElementById("id2").innerHTML = "Hello world...!"
+        // document.getElementById("id2").style.color = "pink";
+    }
+}
+
+{
+    // function findElement()
+    // {
+    // const element = document.getElementById("id1");
+    // element.innerHTML = "Textes...";
+    // element.style.color = "red";
+
+    // document.getElementById("id2").innerHTML = "hello world...".style.color = "red";
+    // }
+}
+//Do this throw Variable declaration
+{
+    const node2 = document.getElementById("id3");
+    {
+        const node1 = document.getElementById("id2");
+        function textNode1() {
+            alert(node1.innerHTML = "this is node inner");
+        }
+        // textNode1();   //If we add here than it give Alert one time default
+    }
+    function textNode2() {
+        alert(node2.innerHTML = "this is node inner");
+    }
+    // textNode2();
+
+}
+
+
+
+
+
+// document.getElementById("id1").innerHTML = "ALERT SHOW...";
+// }
+
+// get elementby tag name
+{
+
+    // const elements = document.getElementsByClassName("blue");
+    // console.log(elements);
+
+    // function changecolor(){
+    //     for(let i = 0 ; i < elements.length ; i++)
+    //         elements[i].style.color = "pink";
+    // }
+
+}
