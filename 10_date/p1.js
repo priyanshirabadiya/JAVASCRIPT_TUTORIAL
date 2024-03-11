fbf963b580f1e55d333c54d48eb74a053fbf5e9d
priyanshirabadiya <139095730+priyanshirabadiya@users.noreply.github.com>
2024-03-12 05:27:14 +0530

    12/3/24

--- 10_date/p1.js
+++ 10_date/p1.js
@@ -4,7 +4,50 @@
 // let d = new Date("2017, 11,10:59:58");
 // console.log(d);
 
-const d = new Date();
-d.getFullYear();
 
-console.log(d);
+// Structure of this Date is : yyyy-mm-dd
+// const d = new Date("5-12-2024");
+// z = d.getFullYear();
+// z = d.getMonth() + 1;
+// console.log(z);
+
+
+// const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+// const d = new Date("2021-03-25");
+// let month = months[d.getMonth()];
+// console.log(month);
+
+// const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+// const d = new Date();
+// let month = months[d.getMonth()];
+// console.log(month);
+
+
+// const d = new Date("2021-03-25");
+// console.log(d.getDate());
+
+
+// const d = new Date();
+// console.log(d.getDate());
+
+// let d = new Date();
+// console.log(d.getHours());
+// console.log(d.getMinutes());
+// console.log(d.getSeconds());
+// console.log(d.getMilliseconds());
+// console.log(d.getDay());
+// console.log(d.getTime());
+// console.log(Date.now());
+
+// const d = new Date();
+// z = d.setSeconds(30);
+// console.log(z);
+
+// const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+// // const d = new Date("2021-03-25");
+// const d = new Date();
+// let day = days[d.getDay()];
+// console.log(day);
