6d8d3ac09a44e66f50191d501c428262df357246
priyanshirabadiya <139095730+priyanshirabadiya@users.noreply.github.com>
2024-03-11 23:22:32 +0530

    3/11/24

---
+++ 10_date/p1.js
@@ -0,0 +1,10 @@
+// it count start date from --> January 1, 1970
+
+// let d = new Date("2018, 11,10:59:58");
+// let d = new Date("2017, 11,10:59:58");
+// console.log(d);
+
+const d = new Date();
+d.getFullYear();
+
+console.log(d);
