3f9b9463b0e8f872db8e0d5e7cdd6b8161bc010d
priyanshirabadiya <139095730+priyanshirabadiya@users.noreply.github.com>
2024-03-17 12:49:11 +0530

    17/3/24

--- projects/calculater/calculater.js
+++ projects/calculater/calculater.js
@@ -1,59 +1,91 @@
-let string ="";
+// let string ="";
 
-let buttons = document.querySelectorAll('.button');
+// let buttons = document.querySelectorAll('.button');
+
+// let arr = Array.from(buttons);
+
+// arr.forEach(x => {
+//     // when someone click then arrow - element function work
+//     x.addEventListener('click',event => {
+//         if(event.target.innerHTML == '='){
+//             string = eval(string);
+//             document.querySelector('input').value = string;
+//         }
+//         else if(event.target.innerHTML == 'AC'){
+//             string = "";
+//             document.querySelector('input').value = string;
+//         }
+//         else if(event.target.innerHTML == 'C'){
+//             string = string.substring(0,string.length - 1);
+//             document.querySelector('input').value = string;
+//         }
+//         else{
+//             console.log(event.target);
+//             string = string + event.target.innerHTML;
+//             document.querySelector('input').value = string;
+//         }
+//     })
+// })
+
+
+
+let txt = "";
+let buttons = document.querySelectorAll(".button");
 
 let arr = Array.from(buttons);
 
 arr.forEach(x => {
-    // when someone click then arrow - element function work
-    x.addEventListener('click',event => {
-        if(event.target.innerHTML == '='){
-            string = eval(string);
-            document.querySelector('input').value = string;
+    x.addEventListener('click',element =>{
+        if(element.target.innerHTML == "="){
+            txt = eval(txt);
+            document.querySelector('input').value = txt;
         }
-        else if(event.target.innerHTML == 'AC'){
-            string = "";
-            document.querySelector('input').value = string;
+        else if(element.target.innerHTML == "AC"){
+            txt = "";
+            document.querySelector('input').value = txt;
         }
-        else if(event.target.innerHTML == 'C'){
-            string = string.substring(0,string.length - 1);
-            document.querySelector('input').value = string;
+        else if(element.target.innerHTML == 'C'){
+            txt = txt.substring(0 , txt.length - 1);
+            document.querySelector('input').value = txt;
         }
         else{
-            console.log(event.target);
-            string = string + event.target.innerHTML;
-            document.querySelector('input').value = string;
+            console.log(element.target);
+            txt = txt + element.target.innerHTML;
+            document.querySelector('input').value = txt;
         }
     })
 })
 
 
 
-// let string = "";
-
-// let buttons = document.querySelectorAll('.button');
-// let arr = Array.from(buttons);
-// arr.forEach(x =>
-//     x.addEventListener('click',value => {
-//         console.log(value.target);
-//     })
-// )
-
-
-// let buttons = document.querySelectorAll('.button');
-
-// let arr = Array.from(buttons);
 
 
 
 
 
 
-// x.addEventListener('click',event =>{
-    //     if(event.target.innerHTML = "="){
-        //         string = eval(string);
-        //     }
-    // })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // let txt = "";
