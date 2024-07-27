ce2a8bbf156c2f49839e08a0b6b59d2f5f366ae6
priyanshirabadiya <rabadiyapriyansi@gmail.com>
2024-07-27 16:07:53 +0530

    27/7/24

--- 20_deep_shellowC/copies.js
+++ 20_deep_shellowC/copies.js
@@ -1,13 +1,135 @@
-// In this code problem is that it is being copy when we assign obj to another variable then 
+// In this code problem is that it is being copy address(memory) when we assign obj to another variable so it change value if we give any other value 
 
-let obj = {
-    name : "peter"
-};
-let user = obj;
-obj.name = "priyanshi";
+// PROBLEM
 
-console.warn(obj);
+// In this obj it will copy memory 
+// let obj = {
+//     name : "peter"
+// };
+// let user = obj;
+// user.name = "priyanshi";
+// console.log(user);
+// console.log(obj);
 
 
+
+// It will not cause any problem because object copy memori Location whereas variable copy its value
+// let x = "anil";
+// let y = x;
+// y = "ridhi";
+// console.log(x);
+// console.log(y);
+
+
+
+// SHALLOW COPY
+// SOLUTION 1
+// let obj = {
+//     name: "Peter",
+//     address: {
+//         city: "Noida",
+//         stat: "UP"
+//     }
+// };
+// let user = Object.assign({},obj);
+// user.name = "Henna";
+// console.log(user);
+// console.log(obj);
+
+
+// SOLUTION 2
+// let obj = {
+//     name: "Peter",
+//     address: {
+//         city: "Noida",
+//         stat: "UP"
+//     }
+// };
+// let users = { ...obj }; // distructured object 
+// users.name = "Henna";
+// console.log(users);
+// console.log(obj);
+
+// Shallow copy consider only first object it is not consider object which is in under of object or within object 
+// following is problem that it change name in user city and obj city also
+// let obj = {
+//     name: "Peter",
+//     address: {
+//         city: "Noida",
+//         stat: "UP"
+//     }
+// };
+
+// let users = { ...obj };   
+// users.address.city = "surat";
+// console.log(users);
+// console.log(obj);
+
+
+
+
+// DEEP COPY
+
+// let obj = {
+//     name: "Peter",
+//     address: {
+//         city: "Noida",
+//         stat: "UP"
+//     },
+//     hello:function(){
+//         return "Hello world";
+//     }
+// };
+// let user = JSON.parse(JSON.stringify(obj));
+// user.address.city = "surat";
+// console.log( "real object" , obj);
+// console.log( "User" , user);
+
+
+
+// By using lodash library : we can see function also right now
+
+// let obj = {
+//     name: "Peter",
+//     address: {
+//         city: "Noida",
+//         stat: "UP"
+//     },
+//     hello:function(){
+//         return "Hello world";
+//     }
+// };
+// let user = _.cloneDeep(obj);
+// user.address.city = "surat";
+// console.log( "real object" , obj);
+// console.log( "User" , user);
+
+
+
+
+
+// Interview questions
+
+// 1. what is perform when we do shallow copy or deep copy ? ans : There is copy value instaed of memory location  
+// 2. Difference between shallow copy and deep copy. ans : shallow copy is just copy only main object whereas in deep copy we can copy nested object also
+
+// 3. How many why to do shallow copy or deep copy
+//  shallow copy => 1. Object.assign()
+//  2. Object destructuring
+// Deep copy => JSON.parse with JSON.STRINGIFY  => in this data and functions are not work to resolve this problem we can use lodash library or manualy you can do using for loop by copying every element to another file  
+
+
+
+// let a = [ 'sanjay' , 'Aman' , 'Rehman' , 'Rahul' ];
+// console.log(a.slice(1,3));
+// console.log(a.slice(2)); // all values after 2
+
+
+// splice(index(position no where you want to insert) , howmany , "new value")
+
+// a.splice(2 , 0 , "kena");
+// a.splice(2 , 1); // sanjay , aman , rahul
+
+// console.log(a); 
 
 
---
+++ 21_InterviewQ/interviewquestion.js
@@ -0,0 +1,96 @@
+
+// 1.what is scripting languahe ?
+// 2.what is javascript ?
+// 3.what is object-oriented programming ?
+// 4.Differences between var , let and const.
+
+
+
+
+
+// -------------------------------------------- copy --------------------------------------------
+
+// 1. what is perform when we do shallow copy or deep copy ?
+// => ans : There is copy value instaed of memory location
+
+
+// 2. Difference between shallow copy and deep copy.
+// =>  shallow copy is just copy only main object whereas in deep copy we can copy nested object also
+
+// 3. How many why to do shallow copy or deep copy ?
+// => shallow copy => 1. Object.assign()
+//  2. Object destructuring
+// Deep copy => JSON.parse with JSON.STRINGIFY  => in this data and functions are not work to resolve this problem we can use lodash library or manualy you can do using for loop by copying every element to another file
+
+
+
+// -------------------------------------------- Map/set --------------------------------------------
+
+// 1. what is map method ?
+// => The map() method in JavaScript is used to create a new array by applying a function to each element of an existing array. It does not modify the original array.
+
+// 2. what is set method ?
+// => Set is a built-in data structure that stores unique values of any type. It provides methods for adding, removing, and checking for elements. Creating a Set.
+
+
+// -------------------------------------------- Null / undefine / NaN --------------------------------------------
+
+// 1. null => Null value represents the intent absence of any object.
+
+// 2. Undefined => A variable that has not been assigned value that is called undefine.
+
+// let a;
+// console.log(a);
+
+// 3. Empty value => An empty has nothing to do with undefined
+
+// 4. NaN => NaN is a number that is not legal number.
+
+
+
+
+// -------------------------------------------- Array slice / splice --------------------------------------------
+
+
+// let a = [ 'sanjay' , 'Aman' , 'Rehman' , 'Rahul' , 'Karan' ];
+// console.log(a.slice(1,3)); // Aman , Rehman // syntax : firstindex of array which value you want to get and lastindex + 1 then and only it will give your acject value
+// console.log(a.slice(2)); // all values after 2nd index
+
+
+// splice
+// splice(index , howmany , "new value")
+// Splice method is LIKE mid insert and mid delete topic of javascript
+
+// let a = [ 'sanjay' , 'Aman' , 'Rehman' , 'Rahul' , 'Karan' ];
+// a.splice(2 , 0 , "kena");
+// a.splice(2 , 1); // sanjay , aman , rahul
+
+// console.log(a); 
+
+
+
+// -------------------------------------------- callbacks --------------------------------------------
+
+// In function we generally pass arguments as parameter but when we pass function as parameter that is called call back function
+
+// function sayhello(){
+//     console.log("Hello");
+// } 
+// function sayhi(){
+//     console.log("Hi..");
+// } 
+
+// function nameF(num1 , num2 , callback) {
+//     console.log(num1 + num2);
+//     callback();
+// }
+
+// nameF(10 , 20 , sayhello);
+// nameF(30 , 20 , sayhi);
+// nameF(10 , 10 , function(){
+//     console.log("Bye...");
+// });
+
+
+
+
