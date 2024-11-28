c45319d0feec2ef6e8d492b4593a6c191b59eb42
priyanshirabadiya <rabadiyapriyansi@gmail.com>
2024-11-28 21:09:24 +0530

    28/11/24

---
+++ paressir_JS/null.js
@@ -0,0 +1,18 @@
+// function test(x = 1) {
+//     console.log(x);
+// }
+
+// test(null);
+// test();
+
+
+// let a = null;
+// console.log(a);
+// a = 10;
+// console.log(a);
+
+
+let a;
+console.log(a);
+a = 10;
+console.log(a);
