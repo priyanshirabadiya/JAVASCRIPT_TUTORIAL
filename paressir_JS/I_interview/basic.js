b5a704a44cb64f459e2c0d66cb21f34201186f46
priyanshirabadiya <rabadiyapriyansi@gmail.com>
2024-12-01 14:49:35 +0530

    1/12/24

--- paressir_JS/I_interview/basic.js
+++ paressir_JS/I_interview/basic.js
@@ -26,6 +26,12 @@
 // JavaScript Implicit Conversion
 // When you use arithmetic operators like - , * , or / with numeric strings, JavaScript automatically changes those strings into numbers. but with + operator it isn't change numeric strings into number so count it as it is as string. Let's look at an example. The table shows the conversion of different values to String , Number , and Boolean in JavaScript.
 
+
+// let result = "3" + 2;
+// let result = "3" - 2;
+// console.log(result);
+// console.log(typeof result);
+
 // let res;
 // // res = "3" + 2;
 // res = "3" + true; // 3true
@@ -36,24 +42,183 @@
 // console.log(typeof res);
 
 // // haivng due to implicit conversion it will count boolean true as 1 or false as 0 
+// console.log(3 + true);
+// console.log(3 - true);
+
+// const data =['sdf', "sdfsd"];
+// data.length = 1;
+// console.log(data[0]);
+
+// 10, 20, 30, 40, 50
+//  0   1   2   3   4
+
+// let arr = [10, 20, 30, 40, 50];
+// console.log(arr);
+// arr[2.1] = 56;
+// // console.log(arr);
+// arr['11'] = 12
+// console.log(arr);
+// // console.log(arr.length);
+
+// let num = '22';
+// console.log(typeof(num));
+
+// console.log(typeof(arr[2]));
+// console.log(typeof(arr));
+// console.log(typeof(arr[8]))
+// console.log(typeof(arr[11]))
 
 
-// hint : consider implicit type conversion
-console.log(3 + true);
-console.log(3 - true);
+// let arr = [10, 20, 30, 40, 50];
+// arr[100] = 400;
+// console.log(arr);
+// console.log(arr.length);
+// arr['105'] = 500;
+// console.log(arr);
+// arr.length = 110;
+// console.log(arr.length);
+// console.log(arr);
 
-const data =['sdf', "sdfsd"];
-data.length = 1;
-console.log(data[0]);
+
+// array of object cannot count length
+// let a = [];
+// for(let i = 0 ; i < 10 ; i++)
+// {
+//     a["i" + i] = i * 100
+// }
+// console.log(a);
+// console.log(a.length);
+
+
+
+// let a = [10 , 20 , 30 , 40 , 50];
+// console.log(a);
+// console.log(a[0],a[1],a[2],a[3],a[-100]);
+
+
+// let a = [];
+// a[0] = 10;
+// console.log(a);
+// console.log(a.length);
+
+
+// let a = [10 , 20 , 30 , 40 , 50];
+// console.log(a);
+// console.log(a.length);
+// delete a[0];
+// console.log(a);
+// console.log(a.length);
+// a[0] = 500;
+// console.log(a);
+
+
+let a = [0];
+// console.log(a);
+// console.log(a[0]);
+// console.log(a[0] == " ");
+// console.log(a[0] === " ");
+
+// console.log(" "*10); // because " " is consided as 0 and multification of 0 with any number is 0 so ans will be 0 
+
+// console.log(typeof(" "));
+// console.log([] == false);
+// console.log(typeof []);
+// console.log([] == []);
+
+// Arrays in JavaScript are objects, and comparisons with == or === check reference equality, not value equality.
+// now in following arr1 and arr3 reference are same in memory where as arr1 and arr2 are diffrent memory locations so it will not return false  
+
+// let arr1 = [];
+// let arr2 = [];
+// let arr3 = arr1;
+// console.log(arr1 === arr3);
+
+// console.log(typeof ["sdsd"]);
+// console.log(typeof []);
+
+// console.log([] == false); // true
+// console.log(" " == false); // true
+// console.log(false == " "); // true
+// console.log(" " === false); // false
+
+// console.log([] == 0 );
+// console.log([] == "0");  // When [] is coerced to a string, it becomes an empty string "".
+// console.log(0 == " ");
+
+
+// console.log(false == " ");
+// console.log([] == 0); // true
+// console.log([] == "0"); // false
+// console.log([0] == 0);
+// console.log(0 === 0);
+// This line compares [0] (an array) with 0 (a number) using the strict equality operator (===).
+
+// Explicit type conversion
+// Explicit coercion in JavaScript is when you intentionally convert a value from one type to another using built-in functions or operators. Here's a breakdown of the common methods:
+
+// Number(): Converts a value to a number.
+// String(): Converts a value to a string.
+// Boolean(): Converts a value to a boolean.
+// parseInt(): Parses a string and returns an integer.
+// parseFloat(): Parses a string and returns a floating-point number.
+
+// console.log(typeof ("5454"));
+// console.log(typeof Number("5454"));
+// console.log(typeof String(533453));
+// console.log(typeof Boolean(533453));
+// console.log(typeof Boolean("sdfdsf"));
+// console.log(typeof parseInt("sdfdsf"));
+
+// console.log(typeof parseInt('10px'));
+// console.log(parseInt('10px'));  // parseInt(): Stops parsing when it encounters a non-numeric character.
+// console.log(typeof parseInt('10px'));  // parseInt(): Stops parsing when it encounters a non-numeric character.
+// console.log(Number('10px')); // Returns NaN (Not a Number) if the entire input cannot be converted to a number.   
+// console.log(typeof Number('10px')); // Returns NaN (Not a Number) if the entire input cannot be converted to a number.   
+// // consider typeof Number and parseInt is always number evenif it don't give result in NaN ncz type of NaN is number
+// console.log(typeof NaN);
+
+// --------------------------------- Array ---------------------------------
+
+// array -> The Array object, as with arrays in other programming languages, enables storing a collection of multiple items under a single variable name, and has members for performing common array operations.
+// ----> print in this way
+// 0    1    2    3    4    5    6    7    8    9
+// 10 , 20 , 30 , 40 , 50 , 60 , 70 , 80 , 90 , 100  
+// -10  -9   -8   -7   -6   -5   -4   -3   -2    -1
+
+// let arr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+// arr.push(500);
+// arr.unshift(500);
+// arr.pop()
+// arr.shift()
+
+// arr.splice(5)
+// arr.splice(8)
+// console.log(arr.splice(8));
+// arr.splice(-3);
+// arr.splice(-8);
+// console.log(arr.splice(-8));  // it's show that this values are removed
+// splice(start index (can conut from 0) , deletecount)
+// console.log(arr.splice(2, 4 , 89 , 78));
+// console.log(arr);
+
+
+
+// slice method
+// 0    1    2    3    4    5    6    7    8    9
+// 10 , 20 , 30 , 40 , 50 , 60 , 70 , 80 , 90 , 100  
+// -10  -9   -8   -7   -6   -5   -4   -3   -2    -1
+
+let arr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+arr.slice(5);
+console.log(arr);
 
 
 
 
+// console.log(arr.splice(2,8));
 
-// let result = "3" + 2;
-// let result = "3" - 2;
-// console.log(result);
-// console.log(typeof result);
+
+
 
 
 
