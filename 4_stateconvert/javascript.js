ad8cb7d2588fca266a4fbaf5029c33f409ca2bda
Priyanshi Rabadiya <rabadiyapriyansi@gmail.com>
2025-06-10 19:08:01 +0530

    -

--- 4_stateconvert/javascript.js
+++ 4_stateconvert/javascript.js
@@ -143,7 +143,6 @@ if...else if...else statement
 
 // Add break statement after default is not necessary but It helps prevent bugs if you or someone else adds more cases below default later. and aslo It makes your code consistent with the other cases.
 
-
 // let str = "r";
 // switch (str) {
 //   case "l":
@@ -174,48 +173,41 @@ parseInt() Parse its argument and returns a whole number
 
 // Number() accepts only "digit strings". For example: "34", "-56", "3.14", "6.022e23", etc. are all valid for Number() While, parseInt() accepts those strings which may or may not have non-digit character(s) at the end of string. For example: "34", "34foo" and "34bar34" will all return the number 34.
 {
-  // let n1 =  Number(true);
-  // let n2 =  Number(false);
-  // let n3 =  Number("10");
-  // let n4 =  Number("  10");
-  // console.log(n4);
-  // let n5=  Number("10  ");
-  // let n6 =  Number(" 10  ");
-  // let n7 =  Number("10.33");
-  // console.log(n7);
-  // console.log(typeof(n7));
-  // let n8 =  Number("10,33");
-  // let n9 =  Number("10 33");
-  //  let n10 =  Number("10John");
-  // console.log(n1);
-  //  console.log(typeof(n1));
-  //  console.log(n10);
-  //  console.log(typeof(n10));
+  //   let n1 =  Number(true);
+  //   let n2 =  Number(false);
+  //   let n3 = Number("10");
+  //   let n4 =  Number("  10");
+  //   let n5=  Number("10  ");
+  //   let n6 =  Number(" 10  ");
+  //   let n7 = Number("10.33");
+  //   let n8 =  Number("10,33");
+  //   let n9 =  Number("10 33");
+  //   let n10 =  Number("10John");
+  //   console.log(n10);
+  //   console.log(typeof n10);
 }
 {
   // let pi1 =  parseInt("-10");
-  // console.log(pi1);
   // let pi2 =  parseInt("-10.33");
   // let pi3 =  parseInt("10");
   // let pi4 =  parseInt("10.33");
-  // console.log(pi4);
-  //  let pi5 =  parseInt("20 30");
-  //  let pi6 =  parseInt("10 years");
-  //  let pi7 =  parseInt("years 10");
-  //  console.log(pi7);
-  //  console.log(typeof(pi7));
+  // let pi5 =  parseInt("20 30");
+  // let pi6 =  parseInt("10 years");
+  // let pi7 = parseInt("years 10");
+  // console.log(pi7);
+  // console.log(typeof pi7);
 }
 {
-  // let pf1 =  parseFloat("10");
-  // let pf2 =  parseFloat("10.33");
-  // let pf3 =  parseFloat("10 20 30");
-  // let pf4 =  parseFloat("10 years");
-  // let pf5 =  parseFloat("years 10");
-  // console.log(pf1);
-  // console.log(pf2);
-  // console.log(pf3);
-  // console.log(pf4);
-  // console.log(pf5);
+//   let pf1 =  parseFloat("10");
+//   let pf2 =  parseFloat("10.33");
+//   let pf3 =  parseFloat("10 20 30");
+//   let pf4 =  parseFloat("10 years");
+//   let pf5 =  parseFloat("years 10");
+//   console.log(pf1);
+//   console.log(pf2);
+//   console.log(pf3);
+//   console.log(pf4);
+//   console.log(pf5);
 }
 
 /* Number Object Method */
