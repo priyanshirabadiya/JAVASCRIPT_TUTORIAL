cdf732f7d701757d95c1a20c5507610282a3f49a
Priyanshi Rabadiya <rabadiyapriyansi@gmail.com>
2025-06-13 23:44:55 +0530

    set() in javascript

--- 7_set/javascript.js
+++ 7_set/javascript.js
@@ -1,11 +1,19 @@
-let fruits = new Set(["apple","banana","kiwi",]);
+// --------------- set method in javascript
+// The Set object lets you store unique values of any type, whether primitive values or object references.
+// It stores only values (no keys)
+// Keys : No keys, only unique values
+// Duplicates : Values must be unique
+
+
+// Set object initialized with values.
+
+// let fruits = new Set(["apple", "banana", "kiwi"]);
 // console.log(fruits);
 
 // has value
 // console.log(fruits.has("apple"));
 
-
-// after clear value being 0 
+// after clear value being 0
 // console.log(fruits.clear());
 // console.log(fruits);
 
@@ -19,14 +27,25 @@ let fruits = new Set(["apple","banana","kiwi",]);
 // entries method
 // console.log(fruits.entries());
 
+// keys method
+// console.log(fruits.keys());
 
-
-
-// keys methos
-// console.log(fruits.keys()); 
-
-// values methos
+// values method
 // console.log(fruits.values());
 
-console.log(fruits.clear());
-console.log(fruits);
+
+// ------------ using empty Set object.
+
+// let mySet = new Set();
+// mySet.add("1");
+// mySet.add("5");
+// mySet.add("3");
+
+// console.log(mySet);
+
+// const uniqueNumbers = new Set();
+// uniqueNumbers.add(1);
+// uniqueNumbers.add(2);
+// uniqueNumbers.add(1); // ignored, already exists
+
+// console.log(uniqueNumbers); // Set { 1, 2 }
