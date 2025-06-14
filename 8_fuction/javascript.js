44854301b695f7c04a7f3aa9c7dd37044016d880
Priyanshi Rabadiya <rabadiyapriyansi@gmail.com>
2025-06-15 00:00:46 +0530

    -

--- 8_fuction/javascript.js
+++ 8_fuction/javascript.js
@@ -1,14 +1,16 @@
 // function -> perform specific task
 
-// function mtfuc(a,b)
-// {
-//     return a * b;
+// function mtfuc(a, b) {
+//   return a * b;
 // }
-// console.log(mtfuc(5,5));
+// console.log(mtfuc(5, 5));
 
+// function FunCtion(a, b) {
+//   return a * b;
+// }
+// console.log(FunCtion(10 , 20));
 
-
-//  Function Expression:-
+// Function Expression:-
 // A JavaScript function can also be defined using an expression.
 // - The function above is an anonymous function (a function without a name).
 // - Functions stored in variables do not need function names. They are always invoked (called) using the variable name.
@@ -20,32 +22,27 @@
 // let z = x(4,5);
 // console.log(z);
 
-
 // function hosting  -->function hoisting means it find value of function at anywhere in the program
-// function myfun(a , b)
-// {
-//     return a*b;
+// function myfun(a, b) {
+//   return a * b;
 // }
-// console.log(myfun(5,2));
-
+// console.log(myfun(5, 2));
 
 // self-involking function
-// (function()
-// {
-//     let x  = "Hello , I called function by myself"
-//     console.log(x);
+// (function () {
+//   let x = "Hello , I called function by myself";
+//   console.log(x);
 // })();
 
-
 // (function()
 // {
-//     let x = 4  
+//     let x = 4
 //     let b = 5 , c
 //     c = x + b;
 //     console.log(c);
 // })();
 
-// direct return value 
+// direct return value
 // function greeting() {
 //     return "Hello!";
 // }
@@ -57,67 +54,82 @@
 // console.log((function (a, b) {
 //     return a + b;
 //    })(2, 3));
-        
-// let x = (function hello(a , b , c ,d){
-//      return{ 
-//      sum: a + b,
-//      sub: c - d
-//     };
-//   }
-// )("5" , 2 , 9 , 8);
+
+// (function printNum(a,b){
+//     console.log(a,b);
+// })(1,2);
+
+// let x = (function hello(a, b, c, d) {
+//   return {
+//     sum: a + b,
+//     sub: c - d,
+//   };
+// })("5", 2, 9, 8);
 // console.log(x);
 
-// function add(a,b)
-// {
-//     return a+b
+// console.log(
+//   (function hello1(a, b) {
+//     return {
+//       sum: a + b,
+//       sub: a - b,
+//     };
+//   })(5, 4)
+// );
+
+// function add(a, b) {
+//   return a + b;
 // }
+// console.log(add(10, 20));
+
 // arrow function
-// var x = add(a,b) => {
-//     return a+b;
+
+// let add = (a, b) => a + b;
+// console.log(add(4, 5));
+
+// --------------------------------------- table
+
+// function table() {
+//   let i, j;
+//   var n = 5;
+//   let m = "";
+//   for (i = 1; i <= 10; i++) {
+//     j = n * i;
+//     m = m + n + " * " + i + " = " + j + "\n";
+//   }
+//   return m;
 // }
+// console.log(table());
 
-// let add = (a,b) => a+b;
-// console.log(add(4,5));
-
-// --------------------------------------------------------------table
-
-// function hello() {
-//     let i, j;
-//     var n = 5;
-//     let m = '';
-//     for (i = 0; i < 10; i++) {
-//         j = n * i;
-//         m = m + n + " * " + i + " = " + j + "\n"; 
-//     }
-//     return m;
+// function table() {
+//   let j;
+//   let n = 5;
+//   for (let i = 1; i <= 10; i++) {
+//     console.log("5" + " * " + i  + " = " + 5*i);
+//   }
 // }
-// console.log(hello());
+// table();
 
-
-// --------------------------------------------------------------function constroctor
+// ----------------------------------function constroctor
 
 // const myFunction = new Function("a", "b", "return a * b");
 // let x = myFunction(4, 3); // 12
 // console.log(x);
 
-
 // const myFunction = new Function("a" , "b", "return a + b");
 // let x = myFunction(2 , 8);
 // console.log(x);
 
-//-----------------arrow
+//----------------- arrow
 
-// var x = (x , y) => x * y;
-// console.log(x(5,10));
-
-
+// var x = (x, y) => x * y;
+// console.log(x(5, 10));
 
 // ---------------------------function are object
 
-// function myFunction(a, b, c, k){
-//     return arguments.length; 
+// function myFunction(a, b, c, k) {
+//   return arguments.length;
 // }
-// let text = myFunction(4,3,5,2);
+// let text = myFunction(4, 3, 5, 2);
 // console.log(text);
 
 // ------------------------------------------------------------------
@@ -129,46 +141,37 @@
 // let len = myfun(1,2,4,5,8,8,7).toString();
 // console.log(len);
 
-// x = func1(1,2,3,4,5,8)
+// x = func1(1, 2, 3, 4, 5, 8);
 // function func1(a, b, c) {
-//     console.log(arguments.length);
+//   console.log(arguments.length);
+// }
+
+// function add(...args) {
+//   let sum = 1;
+//   for (let i = 1; i < args.length; i++) {
+//     sum = sum + args[i];
 //   }
+//   return sum;
+// }
+// console.log(add(1, 2, 5, 8));
 
-
-// function add(...args)
-// {
-//     let sum = 1;
-//     for(let i = 1 ; i <= args.length ; i++)
-//         sum = sum + args[i];
-//         return sum;
-//     }
-//     console.log(add(1,9,10,5,8));
-// console.log(args[i]);
-// add(1,2,5,8)
-
-
-
- 
-//  function sum(...args) {
-//    let sum = 0;
-// for (let arg of args) 
-//             sum += arg;
-// return sum;
+// function sum(...args) {
+//   let sum = 0;
+//   for (let arg of args) sum += arg;
+//   return sum;
 // }
 
 // let x = sum(4, 9, 16, 25, 29, 100, 66, 77); // 326
 // console.log(x);
 
-
 // const person = {
-//    firstName: "John",
-//    lastName: "Doe",
-//    id: 5566,
-//    fullName : function() {
-//                return this.firstName + " " + this.lastName;
-//       }
-// };   // John Doe
+//   firstName: "John",
+//   lastName: "Doe",
+//   id: 5566,
+//   fullName: function () {
+//     return this.firstName + " " + this.lastName;
+//   },
+// }; // John Doe
 // console.log(person.fullName());
 
 // console.log(person);
-
