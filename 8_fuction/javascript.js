c63210e9fac0b8824d6b056d82154c7a7c12cf59
Priyanshi Rabadiya <rabadiyapriyansi@gmail.com>
2025-06-16 13:11:07 +0530

    -

--- 8_fuction/javascript.js
+++ 8_fuction/javascript.js
@@ -14,6 +14,7 @@
 // A JavaScript function can also be defined using an expression.
 // - The function above is an anonymous function (a function without a name).
 // - Functions stored in variables do not need function names. They are always invoked (called) using the variable name.
+// - it is also called - 1. Anonymous function expression 2. Named function expression
 
 // let x = function(a , b)
 // {
@@ -22,7 +23,7 @@
 // let z = x(4,5);
 // console.log(z);
 
-// function hosting  -->function hoisting means it find value of function at anywhere in the program
+// function hosting  --> function hoisting means it find value of function at anywhere in the program
 // function myfun(a, b) {
 //   return a * b;
 // }
@@ -109,7 +110,33 @@
 // }
 // table();
 
-// ----------------------------------function constroctor
+// ---------------------------------- function constroctor
+// In JavaScript, a constructor function is used to create and initialize objects.
+// It acts as a blueprint for creating multiple objects of the same type.
+
+// function Person(name, age) {
+//   this.name = name;
+//   this.age = age;
+//   this.greet = function () {
+//     console.log("Hello, my name is " + this.name);
+//   };
+// }
+
+// let person1 = new Person("John", 30);
+// let person2 = new Person("Alice", 25);
+
+// person1.greet(); // Output: Hello, my name is John
+// person2.greet(); // Output: Hello, my name is Alice
+
+// function PersonName(fname, lname) {
+//     this.fname = fname,
+//     this.lname = lname,
+//     this.fullName = function () {
+//       console.log(`I'm ` + this.fname + " " + this.lname);
+//     }
+// }
+// let x = new PersonName("priyanshi", "rabadiya");
+// x.fullName();
 
 // const myFunction = new Function("a", "b", "return a * b");
 // let x = myFunction(4, 3); // 12
@@ -124,7 +151,11 @@
 // var x = (x, y) => x * y;
 // console.log(x(5, 10));
 
-// ---------------------------function are object
+// --------------------------- The arguments Object(function are object)
+
+// The arguments object is an array-like object available inside non-arrow functions.
+// It contains all the arguments passed to the function, regardless of the function's formal parameters.
+// It's often used to work with functions when the number of arguments is unknown or variable.
 
 // function myFunction(a, b, c, k) {
 //   return arguments.length;
@@ -132,22 +163,24 @@
 // let text = myFunction(4, 3, 5, 2);
 // console.log(text);
 
-// ------------------------------------------------------------------
-
-// function myfun(a,f,d,s)
-// {
-//     return arguments.length;
+// function myfun(a, f, d, s) {
+//   return arguments.length;
 // }
-// let len = myfun(1,2,4,5,8,8,7).toString();
+// let len = myfun(1, 2, 4, 5, 8, 8, 7).toString();
 // console.log(len);
 
-// x = func1(1, 2, 3, 4, 5, 8);
 // function func1(a, b, c) {
-//   console.log(arguments.length);
+//   let sum = 0;
+//   for (let i = 0; i < arguments.length; i++) {
+//     sum += arguments[i];
+//   }
+//   return sum;
 // }
+// x = func1(1, 2, 3, 4, 5, 8);
+// console.log(x);
 
 // function add(...args) {
-//   let sum = 1;
+//   let sum = 0;
 //   for (let i = 1; i < args.length; i++) {
 //     sum = sum + args[i];
 //   }
@@ -160,9 +193,11 @@
 //   for (let arg of args) sum += arg;
 //   return sum;
 // }
-
 // let x = sum(4, 9, 16, 25, 29, 100, 66, 77); // 326
 // console.log(x);
+
+
+// javascipt object and the this Keyword
 
 // const person = {
 //   firstName: "John",
@@ -173,5 +208,4 @@
 //   },
 // }; // John Doe
 // console.log(person.fullName());
-
 // console.log(person);
