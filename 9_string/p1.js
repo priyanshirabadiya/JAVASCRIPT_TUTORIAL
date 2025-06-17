b397da601c1b73f12158d8ad903bf0ba80a8384d
Priyanshi Rabadiya <rabadiyapriyansi@gmail.com>
2025-06-17 19:40:08 +0530

    -

--- 9_string/p1.js
+++ 9_string/p1.js
@@ -6,14 +6,13 @@
 
 // console.log(answer);
 
-
 // let txt = 'hello world';
 // let len = txt.length;
 // console.log(len);
 
 // slice
 // **slice()** extracts a part of a string and returns the extracted part in a new string.
-// It creates new string it will not chages its original string.
+// It creates new string it will not changes its original string.
 
 // The method takes 2 parameters: start position, and end position (end not included).
 // let text = "Apple, Banana, Kiwi";
@@ -26,35 +25,39 @@
 // fruits.slice(2,0);
 // console.log(fruits);
 
-
 // string slice
 // let txt = "12345678910112";
-// let txt2 = "javascript is a language";
+let txt2 = "javascript is a language";
 // console.log(txt.slice(2,10));
 
-// console.log(txt.slice(2,-10)); 
+// console.log(txt.slice(2,-10));
 // The first argument (2) is the start index, meaning it starts at the 3rd character (indexing starts from 0).
 // The second argument (-10) is the end index counted from the end, so:
 // -10 means 10 characters from the end → index 14 - 10 = 4.
 // So this slice is equivalent to: txt.slice(2, 4)
 
-// console.log(txt.slice(-3,-2)); // this series can't move -10 of 0  and can't go on minus side because JavaScript counts positions from zero.
+// console.log(txt.slice(-3, -2)); // 14 - 3 , 14 - 2  // 11 , 12 
+// // this series can't move -10 of 0  and can't go on minus side because JavaScript counts positions from zero.
 // console.log(txt.slice(5));
-// console.log(txt2.slice(-10));
-// console.log(txt2.slice(-10,-5));
+// console.log(txt2.length);
 
-
+// console.log(txt2.slice(-10)); // 24 - 10
+// console.log(txt2.slice(-10,-5)); // 24 - 10 , 24 - 5 => 14 , 19
 
 // substring
+// A substring is just a part of a string. In JavaScript, you can get a part of a string using the .substring() method.
+// It does not change the original string.
+// It returns a new string.
 // (-)count compulsory from 0
-// let text = "Apple,Banana,Kiwi";
-// let data = text.substring(20, -5); // 20 to 0 //aposite position ma lai javu
-// // let data = text.substring(-20, -5); // 0 to 0
-// console.log(data);
+// In JavaScript, .substring(start, end) does NOT support negative numbers.
+// If you use a negative number, JavaScript will treat it as zero.
 
+let text = "Apple,Banana,Kiwi";
+let data = text.substring(2, -3); // 20 to 0
+// let data = text.substring(-20, -5); // 0 to 0
+console.log(data);
 
-
-// substr 
+// substr
 /*
     The difference is that the second parameter specifies the length of the extracted part.
     If you omit the second parameter, substr() will slice out the rest of the string.
@@ -65,7 +68,6 @@
 // console.log(text.substr(7 ,6));
 // console.log(data);
 
-
 // string replace
 // let text = "Please visit Microsoft! And Microsoft is also the best one.";
 // let replaceStr = text.replace(/Microsoft/g, "SkillQode");
@@ -73,20 +75,16 @@
 // console.log(replaceStr);
 // console.log(text);
 
-
-
 // upper lower
 
 // let txt = 'hello WORLD';
 // console.log(txt.toUpperCase());
 // console.log(txt.toLowerCase());
 
-
 // let txt = '     hello world     ';
 // console.log(txt.trim());        //remove space from start - end
 // console.log(txt.trimStart());   //remove space from start
 // console.log(txt.trimEnd());     //remove space from end
-
 
 // pad start
 // let txt = "52";
@@ -96,7 +94,6 @@
 // let arr = [start,end];
 // console.log(arr.join(""));
 
-
 // console.log(start);
 // console.log(end);
 
@@ -105,29 +102,19 @@
 // let padded = text.padStart(4,"0"); // 0005
 // console.log(padded);
 
-
 // let txt = 'hello , world';
 // console.log(txt.split("L"));
 // console.log(txt.split(" "));
-
-
-
-
-
-
 
 // concat string
 // let txt1 = "hello";
 // let txt2 = "world";
 // console.log(txt1.concat(" ",txt2));
 
-
 // charAt
 
 // let txt = "hello pc";
 // console.log(char);
-
-
 
 // let text = "HELLO WORLD l jufn ,kh";
 // let data = text.split(",") // Split on commas
